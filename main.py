5100b2f306e6497edf9d7fb26e3aa206d5bca40b
amine <amine@gmil.com>
2022-02-24 09:07:41 +0100

    ff

--- main.py
+++ main.py
@@ -4,4 +4,27 @@ file = open('claimskg_result.csv')
 csvreader = csv.reader(file)
 
 number_of_claims = sum(1 for row in csvreader)
-print(number_of_claims)
+
+false = 0
+other = 0
+true = 0
+mixture = 0
+with open("claimskg_result.csv") as f:
+    reader = csv.reader(f)
+    for row in reader:
+        list1 = row[9].split()
+        # print(list1)
+        list2 = row[9].split()
+        # print(list2)
+        list_as_lis1 = set(list1)
+        intersection = list_as_lis1.intersection(list2)
+        intersection_as_list = list(intersection)
+        print(intersection_as_list)
+##'''
+list1 = ["amie", "amine", "ami"]
+list2 = ["aemine", "amine", "amieee"]
+
+list_as_lis1 = set(list1)
+intersection = list_as_lis1.intersection(list2)
+intersection_as_list = list(intersection)
+print(intersection_as_list)
