c46283d7a284db07df72b3928fcaf2986504d1ce
amine <amine.elmaroufi@etu.umontpellier.fr>
2022-03-02 17:18:11 +0100

    similarity

--- similarityDetector.py
+++ similarityDetector.py
@@ -1,10 +1,9 @@
 from Levenshtein import distance
 import csv
-edit_dist = distance("Mitt Romney says Barack Obama 'could have gotten crippling sanctions against Iran. He did not.'", "Paul Ryan says the Obama administration ‘watered down sanctions’ against Iran")
-print(edit_dist)
-header = ['TextA', 'TextB', 'IdA', 'IdB', 'Levenshtein-Distance']
-data = []
 
+header = ['IdA', 'IdB', 'TextA', 'TextB', 'LenA', 'LenB', 'Levenshtein-Distance', 'similarity']
+data = []
+###
 with open("small_claimskg_result.csv") as f:
     reader = csv.reader(f)
     myData = list(reader)
@@ -14,10 +13,21 @@ with open("small_claimskg_result.csv") as f:
         for row in myData:
             for row2 in myData:
                 data.clear()
-                data.append(row[1])
-                data.append(row2[1])
                 data.append(row[0])
                 data.append(row2[0])
-                data.append(distance(row[1], row2[1]))
-                writer.writerow(data)
+                data.append(row[1])
+                data.append(row2[1])
+                data.append(len(row[1]))
+                data.append(len(row2[1]))
+                distanced=distance(row[1], row2[1])
+                data.append(distanced)
+                if len(row2[1]) == 0:
+                    quotiant = 0
+                else:
+                    quotiant = (1 - (distanced / max(len(row[1]), len(row2[1])))) * 100
+                if quotiant>50:
+                    data.append(quotiant)
+                    writer.writerow(data)
+
+
 
