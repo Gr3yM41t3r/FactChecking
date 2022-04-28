8634e2230dc58d41b0b3c4d7df47b19c79bf4401
amine <amine@gmil.com>
2022-04-28 11:00:50 +0200

    62

--- fmeasures.py
+++ fmeasures.py
@@ -8,12 +8,16 @@ def precision(classe):
         right_answers = 0
         vraiposifit = 0;
         vraitotal = 0;
+
         for result in results:
+
             if result[9] == result[8] == classe:
                 vraiposifit += 1
             if result[9] == classe:
                 vraitotal += 1
-        #print((vraiposifit / vraitotal) * 100)
+        # print((vraiposifit / vraitotal) * 100)
+        if vraitotal == 0:
+            return 0
         return (vraiposifit / vraitotal) * 100
 
 
@@ -22,26 +26,20 @@ def recall(classe):
         reader = csv.reader(input)
         results = list(reader)
         right_answers = 0
-        vraiposifit = 0;
-        vraitotal = 0;
+        vraiposifit = 0
+        vraitotal = 0
         for result in results:
             if result[9] == result[8] == classe:
                 vraiposifit += 1
             if result[8] == classe:
                 vraitotal += 1
-       # print((vraiposifit / vraitotal) * 100)
+        # print((vraiposifit / vraitotal) * 100)
         return (vraiposifit / vraitotal) * 100
 
 
 def fMeasure(classe):
     prs = precision(classe)
     rcl = recall(classe)
+    if (prs + rcl) == 0:
+        return 0
     return 2 * ((prs * rcl) / (prs + rcl))
-
-
-print("fmeasure", fMeasure("ST"))
-classes=["E","E*","ST","N"]
-total = 0
-for i in classes:
-    total+=fMeasure(i)
-print(total/len(classes))
